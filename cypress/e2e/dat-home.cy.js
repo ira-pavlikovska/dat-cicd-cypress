c72e78994852f21665ca393169d64fff85ced820
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2025-01-12 12:04:24 -0700

    feat: DAT Welcome and Login Tests

--- cypress/e2e/dat-home.cy.js
+++ cypress/e2e/dat-home.cy.js
@@ -1,14 +1,16 @@
-describe('template spec', () => {
-  it('first test', () => {
-    cy.visit('https://example.cypress.io')
+describe('Home Site Overview', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    cy.visit('https://www.dat.com/')
   })
 
-  it('second test', () => {
-    cy.visit('https://example.cypress.io')
+  it('Welcome test', () => {
+    cy.get('h1').contains('WE TAKE THE')
   })
 
-  it('third test', () => {
-    cy.visit('https://example.cypress.io')
+  it('Login test', () => {
+    cy.get('a').contains('Login').click()
+    cy.get('h1').contains('Welcome Back!')
   })
 
 })

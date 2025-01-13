d2fae111951d4cfd023644d0396cdb8c8ca53f46
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2025-01-13 16:04:56 -0700

    test: add sign up test

--- cypress/e2e/dat-home.cy.js
+++ cypress/e2e/dat-home.cy.js
@@ -2,13 +2,17 @@ describe('Home Site Overview', () => {
 
   it('Welcome test', () => {
     cy.visit('https://www.dat.com/')
-    cy.get('h1').contains('WE TAKE THE')
   })
 
   it('Login test', () => {
     cy.visit('https://www.dat.com/')
     cy.get('a').contains('Login').click()
     cy.get('h1').contains('Welcome Back!')
+  })
+
+  it('Signup test', () => {
+    cy.visit('https://www.dat.com/')
+    // todo
   })
 
 })
